633272f556b3a25f6e09cf1d45643e146619c5d7
Marie T <marie.toupuissant@gmail.com>
2023-08-09 12:30:11 +0200

    ✨ F01: Login > Add title & add form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 export default function LoginPage() {
   // State(état, données)
+  const [inputValue, setInputValue] = useState("");
 
+  useState;
   // Comportements
+  const handleSubmit = (event) => {
+    alert(`Bonjour ${inputValue}`);
+    event.preventDefault();
+    setInputValue("");
+  };
 
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
   //Affichage(render)
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h1>Bienvenue chez nous ! </h1>
+      <br />
+      <h2>Connectez-vous </h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button> Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
