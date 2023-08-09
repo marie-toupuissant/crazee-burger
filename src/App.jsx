3aaa0af331ac48562e2f98617256127c69c2f8a1
Marie T <marie.toupuissant@gmail.com>
2023-08-09 12:11:50 +0200

    ✨ F01: LOGIN > Create login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Salut Marie</p>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <LoginPage />;
 }
 
 export default App;
---
+++ src/components/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  // State(état, données)
+
+  // Comportements
+
+  //Affichage(render)
+  return <div>LoginPage</div>;
+}
